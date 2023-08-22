c8c8263eb854fb6eda35a0862e1da9b5dcb8c675
Seung Hong <hsurf22@gmail.com>
2023-08-22 13:40:43 -0700

    add moving banner

--- src/components/PartMainBanner.vue
+++ src/components/PartMainBanner.vue
@@ -1,30 +1,64 @@
 <template>
   <div class="main-banner">
-    <div
-      class="slide-item"
-      style="
-        background-image: url(/data/images/banner/banner-1.jpg);
-        padding: 7rem 0;
-      "
+    <Flicking
+      :options="{
+        align: 'prev',
+        circular: true,
+        autoInit: true,
+        duration: 1000,
+      }"
+      :plugins="plugins"
+      @move-end="onMoveEnd"
     >
-      <b-container>
-        <BannerTextBlock />
-      </b-container>
-    </div>
+      <div
+        class="slide-item"
+        style="
+          width: 100%;
+          background-image: url(/data/images/banner/banner-1.jpg);
+          padding: 7rem 0;
+        "
+      >
+        <b-container>
+          <BannerTextBlock />
+        </b-container>
+      </div>
+      <div
+        class="slide-item"
+        style="
+          width: 100%;
+          background-image: url(/data/images/banner/banner-1.jpg);
+          padding: 7rem 0;
+        "
+      >
+        <b-container>
+          <BannerTextBlock />
+        </b-container>
+      </div>
+    </Flicking>
   </div>
 </template>
 
 <script>
 import BannerTextBlock from "@/components/PartMainBannerText";
+import { Flicking } from "@egjs/vue-flicking";
+import { AutoPlay } from "@egjs/flicking-plugins";
+import "@egjs/vue-flicking/dist/flicking.css";
 
 export default {
   name: "PartMainBanner",
   props: {
     msg: String,
   },
-  components: { BannerTextBlock },
+  components: { BannerTextBlock, Flicking },
   data: () => {
-    return {};
+    return {
+      plugins: [
+        new AutoPlay({ duration: 6000, direction: "NEXT", stopOnHover: true }),
+      ],
+    };
+  },
+  methods: {
+    onMoveEnd() {},
   },
   mounted: () => {},
 };
